b7edda1355009db08586f40438657978aaae3df0
trizzle21 <tdross21@gmail.com>
2017-03-16 00:54:21 -0700

    set up dev and config files

--- afterhacks/__init__.py
+++ afterhacks/__init__.py
@@ -2,6 +2,30 @@
 #
 # 
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__, instance_relative_config=True)
+
+
 app.config.from_object('config')
 app.config.from_pyfile('config.py')
+
+
+#db = SQLAlchemy(app)
+
+
+from flask_assets import Environment, Bundle
+
+# assets = Environment(app)
+# assets.url = app.static_url
+
+# scss = Bundle(
+# 	scss='css/style.scss',
+# 	depends='**/*.scss',
+#     filters=('pyscss',),
+#     output='all.css'
+# )
+
+
+# from . import controllers
+
---
+++ runserver.py
@@ -0,0 +1,11 @@
+#Initializes Server
+
+
+if __name__ == '__main__':	
+	import os
+	from afterhacks import app
+
+
+	app.run(
+	)
+
